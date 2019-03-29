38dc459e92681c0999ca09a5ea77844f459faf1d
Pandey <kispande@38f9d3647b46.ant.amazon.com>
2019-03-29 15:01:23 -0700

    problem1

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -9,7 +9,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  function sum(a, b) { //eslint-disable-line
+    var output = [];
+    output.push(a+b);
+    output.push('The sum of'+a+'and'+b+'is' +output[0]+ '.');
+    return output;  
 }
 
 // Here is the test for sum(); uncomment it to run it
