3ec8c9729f9bc19deeca6c1c0e0e859758a00b38
Pandey <kispande@38f9d3647b46.ant.amazon.com>
2019-03-31 13:49:53 -0700

    solves problem3

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -37,7 +37,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var answer =[];
+  answer.push(sum(a,b)[0]);
+  answer.push(sum(answer.pop(),c)[0]);
+  answer.push(multiply(a,b)[0]);
+  answer.push(multiply(answer.pop(),c)[0]);
+  answer.push(a+' and '+ b+' and '+ c+ ' sum to '+answer[0]+ '.');
+  answer.push('The product of '+ a+' and ' + b+ ' and '+c +' is ' +answer[1]+'.');
+  return answer;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
